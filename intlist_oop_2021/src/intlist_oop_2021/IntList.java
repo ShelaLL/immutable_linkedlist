ac02fe03a8eeebdc8c8ba087b3a1d1518d110ffc
ASUS <ASUS@DESKTOP-SDMPT9S>
2021-03-12 02:12:16 +0100

    Mutable API implemented using an array

--- intlist_oop_2021/src/intlist_oop_2021/IntList.java
+++ intlist_oop_2021/src/intlist_oop_2021/IntList.java
@@ -12,75 +12,16 @@ import java.util.stream.IntStream;
  */
 public class IntList {
 	
-	
-	//Can only be used in the IntList class, so it is a nested class, using static to define
-	private static class Node{
-		private int value;
-		/**
-		 * @peerObject
-		 */
-		//also a representationObject of whatever object the personal representation object are???
-		//peer object of the current node
-		//Node next is the peerObject of a node
-		//All of the node objects are representationObjects of the IntList
-		private Node next;
-		
-		
-		private static boolean hasLength(Node node, int length) {
-			return length == 0? node == null : length > 0 && node != null && hasLength(node.next, length - 1);
-			
-//			if(length == 0)
-//				return node == null;
-//			else
-//				return length > 0 && node != null && hasLength(node.next, length - 1);
-			
-//			condition ? true value : false value
-		} 
-		
-	}
-	
-	//Each value stored by the intList we create a seperate node and we link those nodes into a chain--a linked list. 
-	//And the head field of the intList obejct itself points to first node of that chain.
 	/**
+	 * @invar | elements != null
 	 * @representationObject
 	 */
-	private Node head; 
-	/**
-	 * @invar | Node.hasLength(head, length)
-	 * 
-	 * @representationObject
-	 */
-	private int length; //keep track of the length of the linked list
+	private int[] elements;
 	
-
-	/**
-	 * Returns an array containing the elements of this object
-	 * 
-	 * @creates | result
-	 */
-	//Using the create demands the class to create a new array object every time we called getElements()
 	public int[] getElements() {
-		int[] result = new int[length];
-		Node node = head;
-		int i = 0;
-		while (node != null) {
-			result[i++] = node.value; //put the value of each node into the corresponding array element 
-			node = node.next; //move to the next node
-			//walk over the linked list using the variable
-		}
-		return result;
+		return elements.clone();
 }
 	
-	private static Node createNodes(int[] elements, int index) {
-		if(index == elements.length)
-			return null;
-		else {
-			Node head = new Node();
-			head.value = elements[index];
-			head.next = createNodes(elements, index + 1);
-			return head;
-		}
-	}
 
 	/**
 	 * Initializes this object so that it represents of int values contained in the given array.
@@ -90,8 +31,7 @@ public class IntList {
 	 */
 
 	public IntList(int[] elements){ 
-		this.head = createNodes(elements, 0);
-		this.length = elements.length;
+		this.elements = elements.clone();
 	}
 	
 	/**
@@ -115,24 +55,14 @@ public class IntList {
 	 * The elements that are behind the index shift one position behind to make place for the new element
 	 */
 	public void insertElement(int index, int value) {
-		// we have to find the nodes preceding that index. With the head node, it does not have to be the preceding node. So do not write "Node node = head;"
-		Node sentinel = new Node();
-		sentinel.next = head;
-		Node node = sentinel;
-		//This will be the node preceding the first node. If want to insert before the first node, we will have to change the next pointer of the sentinel node
-		while(0 < index) {
-			node = node.next;
-			index--;
-		}
-		//So now we are pointing using variable node to the linked-list node preceding the one where we want to insert, preceding the location where we want to insert
-		Node newNode = new Node();
-		newNode.value = value;
-		newNode.next = node.next; 
-		node.next = newNode;
-		//So we now insert the newNode in the right position to implement the insertElement method
-		head = sentinel.next;
-		length++;
+		int[] newElements = new int[elements.length + 1];
+		System.arraycopy(elements, 0, newElements, 0, index);
+		//copy the elements from the old array, index 0, newElements, index 0, how many - index)
+		newElements[index] = value;
+		System.arraycopy(elements, index, newElements, index + 1, elements.length - index);
 		
+		//install this new array into our new representationObject
+		elements = newElements;
 	}
 	
 	/**
@@ -150,12 +80,7 @@ public class IntList {
 	 */
 	//int can never be null, so do not need to write the precondition that index/value != null
 	public void setElement(int index, int value) {
-		Node node = head;
-		while (0 < index) {
-			node = node.next;
-			index--;
-		}
-		node.value = value;
+		elements[index] = value;
 	}
 	
 	
@@ -172,15 +97,9 @@ public class IntList {
 	 */
 	
 	public void removeElement (int index) {
-		Node sentinel = new Node();
-		sentinel.next = head;
-		Node node = sentinel;
-		while ( 0 <  index) {
-			node = node.next;
-			index--;
-		}
-		node.next = node.next.next;
-		head = sentinel.next;
-		length--;
+		int[] newElements = new int[elements.length - 1];
+		System.arraycopy(elements, 0, newElements, 0, index);
+		System.arraycopy(elements, index + 1, newElements, index, newElements.length - index);		
+		elements = newElements;
 	}	
 }
